90d6bda267af3730f552f17aae6c8ec88e95f348
salbiyath <salbiyat@gmail.com>
2023-04-19 15:43:23 +0700

    Add product repository

---
+++ src/main/java/com/salbiyath/pagination/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.salbiyath.pagination.repository;
+
+import com.salbiyath.pagination.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+
+    Page<Product> findAll(Pageable pageable);
+
+}
